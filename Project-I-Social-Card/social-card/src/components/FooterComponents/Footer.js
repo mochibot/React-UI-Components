7d1c1d3293e86db7740387bf984f7bd696bed4d3
mochibot <amusaur@gmail.com>
2019-05-28 00:32:06 -0700

    attempted stretch goal by re-writing Footer component to increment count when the share or like icons are clicked

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -6,6 +6,56 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+/* 
+Re-wrote footer component to add functionality which updates the number of shares and likes when icons are clicked
+*/
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shares: 0,
+      likes: 0
+    }
+    this.shareClick = this.shareClick.bind(this);
+    this.likeClick = this.likeClick.bind(this);
+  } 
+  shareClick() {
+    this.setState({
+      shares: this.state.shares + 1
+    })
+  }
+
+  likeClick() {
+    this.setState({
+      likes: this.state.likes + 1
+    })
+  }
+
+  render() {
+    return (
+      <div className={'footer-container'}>
+      <div className={'icon icon-comment'}>
+        <FontAwesomeIcon icon={faComment} flip={'horizontal'} />
+      </div>
+      <div className={'icon icon-share'}>
+        <FontAwesomeIcon icon={faSync} onClick={this.shareClick} />
+        <p>{this.state.shares}</p>
+      </div>
+      <div className={'icon icon-like'}>
+        <FontAwesomeIcon icon={faHeart} onClick={this.likeClick}/>
+        <p>{this.state.likes}</p>
+      </div>
+      <div className={'icon icon-email'}>
+        <FontAwesomeIcon icon={faEnvelope}/>
+      </div>
+    </div>
+
+    )
+  }
+}
+
+/* Commenting out earlier code
+
 const Footer = () => {
   return (
     <div className={'footer-container'}>
@@ -27,7 +77,7 @@ const Footer = () => {
   )
 }
 
-
+*/
 
 
 export default Footer;
